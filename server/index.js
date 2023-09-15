7446cdc226793f0b976f66c7db11734c38347d09
Leandro Gabriel <leogabrields97@gmail.com>
2023-09-15 13:59:06 -0300

    connecting the front-end with the back-end

--- server/index.js
+++ server/index.js
@@ -2,14 +2,25 @@ import cors from "cors"
 import express from "express"
 
 import { download } from "./download.js"
+import { transcribe } from "./transcribe.js"
+import { summarize } from "./summarize.js"
 
 const app = express()
+app.use(express.json())
 app.use(cors())
 
-app.get("/summary/:id", (req, res) => {
-  download(req.params.id)
+app.get("/summary/:id", async (req, res) => {
+  await download(req.params.id)
 
-  res.json({ result: "Download do vídeo realizado com sucesso!" })
+  const result = await transcribe()
+
+  return res.json({ result })
+})
+
+app.post("/summary", async (req, res) => {
+  const result = await summarize(req.body.text)
+
+  return res.json({ result })
 })
 
 app.listen(3333, () => console.log("Server is running http://localhost:3333/"))
